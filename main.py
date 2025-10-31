02dec287b4cead2a9bce816c89b801bfa1814556
tejascodex001 <tejas12gowda@gmail.com>
2025-10-31 16:43:18 +0530

    basic fastapipost implementation

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Request(BaseModel):
+    Prompt : str
+    Language : str
+
+app = FastAPI()
+
+@app.post("/Plan")
+def root(lang:Request):
+    return (f"Planning solution for {lang.Language}")
+
+# @app.get("/Plan?Language=cpp")
